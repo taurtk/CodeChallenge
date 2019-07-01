bdb5b69a9f7747a43a86fbb2fdd088a777d5a464
Taufique Khan <36895499+taurtk@users.noreply.github.com>
2019-07-02 00:17:41 +0530

    Update findduplicate.py

--- findduplicate.py
+++ findduplicate.py
@@ -1,10 +1,11 @@
+# coding: utf-8
+# Your code here!
 a=[1,1,2,2,2,3,4,5]
 i=0
-b=set()
-
+a.sort()
 while i<len(a):
-    if a[i] in b:
+    if a[i]==a[i-1]:
         print(a[i])
-    else:
-        b.add(a[i])
     i=i+1
+    
+
